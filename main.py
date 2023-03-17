a63272d778bbe2496e122215ef6226a421d546a9
andrewmedved <andrew777tesla@gmail.com>
2023-03-17 18:09:50 +0300

    Make some circles

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-Hello, game!
+print("Hello, game!")
+
+logic_rule = 'yes'
+counter = 0
+
+while logic_rule == 'yes':
+	logic_rule = input("Continue our cicle? ")
+	print(counter)
+	counter += 1
+
+print("The last iteration {}".format(counter))
+print("End of the cicle.\nGoodbye!!!")
+
+
